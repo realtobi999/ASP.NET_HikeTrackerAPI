e17eebc4bf4f22ead71828054593005a38431aac
realtobi999 <tobiasfilgas@gmail.com>
2024-04-29 13:35:21 +0200

    [feat] CORS

---
+++ src/HikingTracks.Presentation/Extensions/ServiceExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace HikingTracks.Presentation.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureCors(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+                    builder
+                    .AllowAnyOrigin()
+                    .WithMethods("POST", "GET", "PUT", "DELETE"));
+        });
+    }
+}
--- src/HikingTracks.Presentation/Program.cs
+++ src/HikingTracks.Presentation/Program.cs
@@ -1,25 +1,25 @@
+using HikingTracks.Presentation.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.ConfigureCors();
 }
 
-app.UseHttpsRedirection();
+var app = builder.Build();
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-app.UseAuthorization();
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
 
-app.MapControllers();
-
-app.Run();
+    app.Run();
+}
