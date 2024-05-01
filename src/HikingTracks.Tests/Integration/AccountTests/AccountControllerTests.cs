3a0b63a40c2dee9cf982206386715a1ffa958547
realtobi999 <tobiasfilgas@gmail.com>
2024-05-01 12:18:38 +0200

    [test][integration] tested all current endpoints

---
+++ src/HikingTracks.Tests/Integration/AccountTests/AccountControllerTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.Http.Json;
+using FluentAssertions;
+using HikingTracks.Domain.DTO;
+using HikingTracks.Domain.Entities;
+using HikingTracks.Domain.Exceptions;
+using HikingTracks.Presentation;
+using Xunit.Sdk;
+
+namespace HikingTracks.Tests.Integration.AccountTests;
+
+public class AccountControllerTests
+{
+    [Fact]
+    public async Task Account_GetAccounts_ReturnsEmptyWhenNoAccounts()
+    {
+        var client = new WebAppFactory<Program>().CreateDefaultClient();
+        var response = await client.GetAsync("/api/account");
+
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        (await response.Content.ReadFromJsonAsync<List<AccountDto>>()).Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task Account_GetAccount_Works()
+    {
+        var client = new WebAppFactory<Program>().CreateDefaultClient(); 
+        var account = new Account().WithFakeData();
+
+        var create = await  client.PostAsJsonAsync("/api/account", account);
+        create.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+
+        var response = await client.GetAsync(string.Format("/api/account/{0}", account.ID));
+
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        var responseBody = await response.Content.ReadFromJsonAsync<AccountDto>() ?? throw new Exception("Failed to deserialize the response body into an AccountDto object.");
+        responseBody.ID.Should().Be(account.ID);
+    }
+
+    [Fact]
+    public async Task Account_CreateAccount_Works()
+    {
+        var client = new WebAppFactory<Program>().CreateDefaultClient();
+        var account = new Account().WithFakeData();
+
+        var response = await  client.PostAsJsonAsync("/api/account", account);
+
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+        response.Headers.Contains("Location").Should().BeTrue();
+
+        var header = response.Headers.GetValues("Location");
+
+        header.Should().Equal(string.Format("/api/account/{0}", account.ID));
+    }
+}
---
+++ src/HikingTracks.Tests/Integration/WebAppFactory.cs
@@ -0,0 +1,40 @@
+﻿using HikingTracks.Infrastructure;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HikingTracks.Tests;
+
+public class WebAppFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+{
+    private readonly string _dbName = Guid.NewGuid().ToString();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        // Configure services for the test web host
+        builder.ConfigureServices(services =>
+        {
+            // Replace the DbContext registration with an in-memory database
+            this.ReplaceDbContextWithInMemoryDb(services);
+        });
+    }
+
+    private void ReplaceDbContextWithInMemoryDb(IServiceCollection services)
+    {
+        // Remove the existing DbContext registration
+        var descriptor = services.SingleOrDefault(
+            d => d.ServiceType == typeof(DbContextOptions<HikingTracksContext>));
+
+        if (descriptor != null)
+        {
+            services.Remove(descriptor);
+        }
+
+        // Add DbContext with an in-memory database
+        services.AddDbContext<HikingTracksContext>(options =>
+        {
+            options.UseInMemoryDatabase(_dbName);
+        });
+    }
+}
