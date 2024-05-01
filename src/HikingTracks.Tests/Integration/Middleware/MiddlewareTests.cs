6936d0a589619b4ac8c9a857c0031fd8a6d67df9
realtobi999 <tobiasfilgas@gmail.com>
2024-05-01 17:32:27 +0200

    [test] test for global error handling works properly

---
+++ src/HikingTracks.Tests/Integration/Middleware/MiddlewareTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http.Json;
+using FluentAssertions;
+using HikingTracks.Domain.Entities;
+using HikingTracks.Presentation;
+
+namespace HikingTracks.Tests.Integration.Middleware;
+
+public class MiddlewareTests
+{
+    [Fact]
+    public async Task App_TestThatGlobalErrorHandlingWorksAsync()
+    {
+        var client = new WebAppFactory<Program>().CreateDefaultClient();
+        var response = await client.GetAsync(string.Format("/api/account/{0}", Guid.Empty));
+
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
+        var body = await response.Content.ReadFromJsonAsync<ErrorDetails>() ?? throw new Exception("Failed to deserialize the response body into an AccountDto object.");
+
+        body.StatusCode.Should().Be(404);
+        body.Message.Should().Be("The account with the id: 00000000-0000-0000-0000-000000000000 doesn't exist.");
+    }
+}
