ad5bb5b71e37f842449fdc5ebbfb51cec76120ba
realtobi999 <tobiasfilgas@gmail.com>
2024-05-01 17:11:30 +0200

    [test] more unit tests for account service

--- src/HikingTracks.Tests/Unit/AccountServiceTests/AccountServiceTests.cs
+++ src/HikingTracks.Tests/Unit/AccountServiceTests/AccountServiceTests.cs
@@ -11,6 +11,38 @@ namespace HikingTracks.Tests.Unit.AccountServiceTests;
 public class AccountServiceTests
 {
     [Fact]
+    public async void Account_GetAllAccount_Works()
+    {
+        var account1 = new Account().WithFakeData();
+        var account2 = new Account().WithFakeData();
+        var repository = new Mock<IRepositoryManager>();
+        var logger = new Mock<ILoggerManager>();
+        repository.Setup(repo => repo.Account.GetAllAccounts()).ReturnsAsync([account1, account2]);
+        var service = new AccountService(repository.Object, logger.Object);
+
+        var accounts = await service.GetAllAccounts();
+
+        accounts.Should().NotBeEmpty();
+        accounts.ElementAt(0).Should().BeEquivalentTo(account1.ToDTO());
+        accounts.ElementAt(1).Should().BeEquivalentTo(account2.ToDTO());
+    }
+
+    [Fact]
+    public async void Account_GetAccount_Works()
+    {
+        var account = new Account().WithFakeData();
+        var repository = new Mock<IRepositoryManager>();
+        var logger = new Mock<ILoggerManager>();
+        repository.Setup(repo => repo.Account.GetAccount(account.ID)).ReturnsAsync(account);
+        var service = new AccountService(repository.Object, logger.Object);
+
+        var retrievedAccount = await service.GetAccount(account.ID);
+    
+        retrievedAccount.Should().NotBeNull();
+        retrievedAccount.Should().BeEquivalentTo(account.ToDTO());
+    }
+
+    [Fact]
     public async void Account_GetAccount_FailsWhenNotFound()
     {
         var repository = new Mock<IRepositoryManager>();
