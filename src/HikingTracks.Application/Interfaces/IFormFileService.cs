ea39ff0e43f27a0c2142baa57808b254e680b8a6
Tobiáš Filgas <tobiasfilgas@gmail.com>
2024-05-10 00:23:52 +0200

    [feat] form file service

---
+++ src/HikingTracks.Application/Interfaces/IFormFileService.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace HikingTracks.Application;
+
+public interface IFormFileService
+{
+    Task<byte[]> IntoByteArray(IFormFile file);
+}
---
+++ src/HikingTracks.Application/Service/FormFileService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace HikingTracks.Application;
+
+public class FormFileService : IFormFileService
+{
+    public async Task<byte[]> IntoByteArray(IFormFile file)
+    {
+        using var stream = new MemoryStream();
+        await file.CopyToAsync(stream);
+
+        // Convert the file if its longer than 2 MB
+        if (stream.Length < 2097152)
+        {
+            return stream.ToArray();
+        }
+        else
+        {
+            throw new Exception($"Photo: {file.FileName} is bigger than 2 MB.");
+        }
+    }
+}
