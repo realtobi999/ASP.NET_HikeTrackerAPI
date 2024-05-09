ca3f1c8d64666e41850a1f72f55edd3e0bf2ca74
Tobiáš Filgas <tobiasfilgas@gmail.com>
2024-05-10 01:16:11 +0200

    [refactor] cleaner way of writing the IntoByteArray

--- src/HikingTracks.Application/Service/FormFileService.cs
+++ src/HikingTracks.Application/Service/FormFileService.cs
@@ -6,17 +6,20 @@ public class FormFileService : IFormFileService
 {
     public async Task<byte[]> IntoByteArray(IFormFile file)
     {
-        using var stream = new MemoryStream();
-        await file.CopyToAsync(stream);
+        using (var stream = new MemoryStream())
+        {
+            await file.CopyToAsync(stream);
 
-        // Convert the file if its longer than 2 MB
-        if (stream.Length < 2097152)
-        {
-            return stream.ToArray();
-        }
-        else
-        {
-            throw new Exception($"Photo: {file.FileName} is bigger than 2 MB.");
+            // Check if the file size is within limits
+            if (stream.Length < 2097152) // 2 MB in bytes
+            {
+                return stream.ToArray();
+            }
+            else
+            {
+                throw new Exception($"File '{file.FileName}' is bigger than 2 MB.");
+            }
         }
     }
 }
+
